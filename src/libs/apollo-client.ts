0dd435b77abd20f623fec7e6e3589f12d3ea454b
Boris Ohorodniy <julijaogr@ukr.net>
2025-07-11 17:24:15 +0300

    remove cache apollo client

--- src/libs/apollo-client.ts
+++ src/libs/apollo-client.ts
@@ -1,4 +1,74 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import {
+	ApolloCache,
+	ApolloClient,
+	NormalizedCacheObject,
+	createHttpLink
+} from '@apollo/client'
+
+const emptyCacheObj = {}
+class VoidCache extends ApolloCache<NormalizedCacheObject> {
+	read(options) {
+		return null
+	}
+	write(options) {
+		return undefined
+	}
+	diff(options) {
+		return {}
+	}
+	watch(watch) {
+		return () => {}
+	}
+	async reset() {}
+	evict(options) {
+		return false
+	}
+	restore(data) {
+		return this
+	}
+	extract(optimistic) {
+		return emptyCacheObj
+	}
+	removeOptimistic(id) {}
+	batch(options) {
+		return undefined as any
+	}
+	performTransaction(update, optimisticId) {}
+	recordOptimisticTransaction(transaction, optimisticId) {}
+	transformDocument(document) {
+		return document
+	}
+	transformForLink(document) {
+		return document
+	}
+	identify(object) {
+		return undefined
+	}
+	gc() {
+		return [] as string[]
+	}
+	modify(options) {
+		return false
+	}
+	readQuery(options, optimistic?) {
+		return null
+	}
+	readFragment(options, optimistic?) {
+		return null
+	}
+	writeQuery(opts) {
+		return undefined
+	}
+	writeFragment(opts) {
+		return undefined
+	}
+	updateQuery(options, update) {
+		return null
+	}
+	updateFragment(options, update) {
+		return null
+	}
+}
 
 const httpLink = createHttpLink({
 	uri: process.env.NEXT_PUBLIC_SERVER_URL,
@@ -7,5 +77,15 @@ const httpLink = createHttpLink({
 
 export const client = new ApolloClient({
 	link: httpLink,
-	cache: new InMemoryCache()
+	cache: new VoidCache(),
+	defaultOptions: {
+		watchQuery: {
+			fetchPolicy: 'no-cache',
+			errorPolicy: 'ignore'
+		},
+		query: {
+			fetchPolicy: 'no-cache',
+			errorPolicy: 'all'
+		}
+	}
 })
