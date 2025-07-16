c5980d6a87a67a237a2ed69e4aa0b91a4c4a02d1
Boris Ohorodniy <julijaogr@ukr.net>
2025-07-16 19:47:38 +0300

    fix names

--- src/app/(site)/process/hsts-mvs/[id]/page.tsx
+++ src/app/(site)/process/hsts-mvs/[id]/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next'
+
 import HstsMvsProfilePage from '@/components/pages/HstsMvsProfilePage'
+
+export const metadata: Metadata = {
+	title: 'Профіль запиту'
+}
 
 export default function HstsMvsProfile() {
 	return <HstsMvsProfilePage />
