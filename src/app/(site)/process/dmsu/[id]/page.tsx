0f7e161b49b492494d32688aa3baafd2b3e17e64
Boris Ohorodniy <julijaogr@ukr.net>
2025-07-23 16:58:52 +0300

    fix

--- src/app/(site)/process/dmsu/[id]/page.tsx
+++ src/app/(site)/process/dmsu/[id]/page.tsx
@@ -1,10 +1,7 @@
 'use client'
 
-import { Permission } from '@/graphql/generated/output'
-
 import { DmsuProcessView } from '@/features/process'
 import { withAuth } from '@/features/shared/hocs'
-import { BorderLayout } from '@/shared/components/layouts'
 
 const AuthenticatedDmsuProfile = withAuth(function DmsuProfileContent() {
 	return <DmsuProcessView />
